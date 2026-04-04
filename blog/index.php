169bcc3135e1b8c3d9cbf794065eadecec71d97c
jay nishar <jaynishar619@gmail.com>
2026-04-04 17:46:53 +0530

    fix: eliminate blog/ folder causing 403 on Hostinger
    - Move all blog articles from blog/ folder to root level
    - Delete blog/ folder entirely - no more directory listing 403
    - Rewrite .htaccess with clean explicit routes, no !-f/!-d conflicts
    - All /blog/slug URLs now route to root-level .php files

--- blog/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This file exists so Apache doesn't try to list the blog/ directory (which causes 403).
-// It simply includes the parent blog.php which is the actual Insights page.
-include __DIR__ . '/../blog.php';
