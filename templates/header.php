c2e6ef36790ea3d6a145d65efae1922dad50f011
jay nishar <87682692+jaynishar@users.noreply.github.com>
2026-09-24 22:49:13 +0530

    Revert email back to devdoshi@corpeasy.in except in privacy policy

--- templates/header.php
+++ templates/header.php
@@ -250,7 +250,7 @@ $page_lcp_image = $page_lcp_image ?? '';
             "height": 461
           },
           "telephone": "+919833089993",
-          "email": "office@corpeasy.in",
+          "email": "devdoshi@corpeasy.in",
           "foundingDate": "2025-10",
           "foundingLocation": "Mumbai, Maharashtra, India",
           "founder": [
@@ -362,7 +362,7 @@ $page_lcp_image = $page_lcp_image ?? '';
             "height": 192
           },
           "telephone": "+919833089993",
-          "email": "office@corpeasy.in",
+          "email": "devdoshi@corpeasy.in",
           "foundingDate": "2025-10",
           "foundingLocation": "Mumbai, Maharashtra, India",
           "address": {
